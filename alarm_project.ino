622a0cee17f8cfd1f0c245dc8e1757eb07fca009
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2024-12-27 18:27:33 +0530

    Update alarm_project.ino

--- alarm_project.ino
+++ alarm_project.ino
@@ -6,22 +6,22 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 RTC_DS3231 rtc;
 
 int reset_button = 8;
-int alarm_set_button = 9;
+int alarm_reset_button = 9;
 int switch_button = 10;
 
-  void setup() {
+DateTime = rtc.now();
+
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  pinMode(alarm_set_button, INPUT);
+  pinMode(alarm_reset_button, INPUT);
   pinMode(reset_button, INPUT);
   pinMode(switch_button, INPUT);
 
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
-    while (1)
-      ;
   }
 
   if (rtc.lostPower()) {
@@ -31,20 +31,52 @@ int switch_button = 10;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  lcd.setCursor(0, 0);
-  lcd.print("Time:");
 
-  lcd.setCursor(0, 1);
-  lcd.print(hour());
-  lcd.print(":");
-  lcd.print(minute());
-  lcd.print(":");
-  lcd.print(second());
+  while (digitalRead(reset_button) != 1 || digitalRead(alarm_reset_button) != 1) {
+    lcd.setCursor(0, 0);
+    lcd.print("Time: ")
+      regular_clock_mode();
+  }
+
+  if (digitalRead(reset_button) == 1) {
+    lcd.clear();
+    delay(1000);
+    reset_normal_clock();
+  }
+
+  else if (digitalRead(alarm_reset_button) == 1) {
+    lcd.clear();
+    delay(2000);
+    lcd.setCursor(0, 0);
+    lcd.print("Set alarm: ");
+
+    set_alarm();
+  }
 }
 
-void reset_clock() {
+//if reset is high, set back the alarm to 00:00
+
+//if set_alarm is high, set clock to 00:00:00 and then make use of increment and decrement operations
+
+//if alarm_start is high, return to regular clock and countdown
+
+void reset_normal_clock() {
+  rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second()));
+}
+
+void reset_alarm_clock() {
 }
 
 void set_alarm() {
+  lcd.setCursor(1, 2);
+  lcd.print("00 : 00");
+}
+
+void regular_clock_mode() {
+  lcd.setCursor(1, 2);
+  lcd.print(now.hour());
+  lcd.print(":");
+  lcd.print(now.minute());
+  lcd.print(":");
+  lcd.print(now.second());
 }
