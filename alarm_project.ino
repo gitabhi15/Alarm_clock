ab4e58892fe9a4470c2b88187629d2d3e468a26f
Abhiram Mandadi <142098886+gitabhi15@users.noreply.github.com>
2025-10-15 14:16:51 +0530

    Baseline alarm model ready to deploy
    This baseline model essentially captures the basic functionalities of the alarm clock. It has an accurate real-time display, a basic alarm feature (absolute only), functional keypad interaction, and a buzzer that sets off the alarm sound.

--- alarm_project.ino
+++ alarm_project.ino
@@ -1,178 +1,242 @@
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal.h>
 #include <Wire.h>
 #include <RTClib.h>
 #include <DS3232RTC.h>
+#include <Keypad.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+#define ROWS 4
+#define COLS 4
+
+byte rowPins[ROWS] = { 36, 38, 40, 42 };
+byte colPins[COLS] = { 44, 46, 48, 50 };
+
+char keys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+LiquidCrystal lcd(22, 24, 26, 28, 30, 32);
 RTC_DS3231 rtc;
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-int clk_reset_button = 8;
-int alarm_reset_button = 9;
-int increment_button = 10;
-int decrement_button = 11;
-int confirm_alarm_button = 3;
-int alarm_mode_button = 4;
+const int buzzPin = 11;
+const int sqwPin = 2;
 
+// Flags
+volatile bool alarmTriggered = false;  // set in ISR
+bool alarmMode = false;
+bool settingMinutes = false;
+bool settingHours = false;
+bool showBootUp = false;
+bool alarmReady = false;
+
+// State variables
+String mins = "";
+String hrs = "";
+unsigned long prevClockUpdate = 0;
+const unsigned long clockUpdateInterval = 1000;
+unsigned long bootUpStartTime = 0;
+const unsigned long bootUpDuration = 4000;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  lcd.init();
-  lcd.backlight();
+  pinMode(buzzPin, OUTPUT);
+  pinMode(sqwPin, INPUT_PULLUP);
 
-  pinMode(alarm_reset_button, INPUT);
-  pinMode(clk_reset_button, INPUT);
-  pinMode(increment_button, INPUT);
-  pinMode(decrement_button, INPUT);
-  pinMode(confirm_alarm_button, INPUT);
-  pinMode(alarm_mode_button, INPUT);
+  lcd.begin(16, 2);
 
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
+    while (1)
+      ;
   }
 
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, setting the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
+  // Clear alarms and configure SQW pin for alarm interrupt mode
+  rtc.clearAlarm(1);
+  rtc.disableAlarm(2);
+  rtc.writeSqwPinMode(DS3231_OFF);
+
+  // Attach ISR for SQW pin (fires when alarm goes off, active LOW)
+  attachInterrupt(digitalPinToInterrupt(sqwPin), alarmISR, FALLING);
+  //syncClock();
 }
 
 void loop() {
+  // Regular time display every second
+  if (millis() - prevClockUpdate >= clockUpdateInterval && !alarmMode) {
+    prevClockUpdate = millis();
+    displayClock();
+  }
 
-  DateTime currTime = rtc.now();
+  char key = keypad.getKey();
+  if (key != NO_KEY) {
+    handleKeyPress(key);
+  }
+
+  if (alarmMode) {
+    handleAlarmSetting();
+  }
+
+  // Transition from boot-up screen to minutes input
+  if (showBootUp && millis() - bootUpStartTime >= bootUpDuration) {
+    lcd.clear();
+    showBootUp = false;
+    settingMinutes = true;
+  }
+
+  // If alarm has been triggered by RTC
+  if (alarmTriggered) {
+    soundAlarm();
+  }
+}
+
+void handleKeyPress(char key) {
+  // Enter alarm mode
+  if (!alarmMode && key == 'A') {
+    mins = "";
+    hrs = "";
+    alarmMode = true;
+    showBootUp = true;
+    bootUpStartTime = millis();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("C to confirm");
+    lcd.setCursor(0, 1);
+    lcd.print("# to cancel");
+    return;
+  }
+
+  // Handle alarm setting inputs (only after entering alarm mode)
+  if (alarmMode && !showBootUp) {
+    if (settingMinutes) {
+      if (key == '#') {
+        mins = "";
+        clearRow(0);
+      } else if (key == 'C') {
+        settingMinutes = false;
+        settingHours = true;
+        clearRow(0);
+      } else if (isdigit(key)) {
+        if (mins.length() < 2) {
+          mins += key;
+        }
+      }
+    } else if (settingHours) {
+      if (key == '#') {
+        hrs = "";
+        clearRow(0);
+      } else if (key == 'C') {
+        alarmReady = true;
+      } else if (isdigit(key)) {
+        if (hrs.length() < 2) {
+          hrs += key;
+        }
+      }
+    }
+  }
+
+  // Optional: allow canceling alarm mode before finishing
+  if (alarmMode && key == 'D') {
+    lcd.clear();
+    lcd.print("Alarm canceled");
+    delay(1000);
+    lcd.clear();
+    resetAlarmState();
+  }
+}
+
+
+void handleAlarmSetting() {
+  if (settingMinutes) {
+    lcd.setCursor(0, 0);
+    lcd.print("Minutes: ");
+    lcd.setCursor(9, 0);
+    lcd.print(mins + "  ");
+  } else if (settingHours) {
+    lcd.setCursor(0, 0);
+    lcd.print("Hours: ");
+    lcd.setCursor(9, 0);
+    lcd.print(hrs + "  ");
+  }
+
+  if (alarmReady) {
+    int m = mins.toInt();
+    int h = hrs.toInt();
+    DateTime now = rtc.now();
+
+    // Set Alarm1 for today at H:M:00
+    rtc.setAlarm1(DateTime(now.year(), now.month(), now.day(), h, m, 0), DS3231_A1_Hour);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Alarm Set to:");
+    lcd.setCursor(4, 1);
+    lcd.print(h);
+    lcd.print(" : ");
+    lcd.print(m);
+    delay(2000);
+    lcd.clear();
+
+    resetAlarmState();
+  }
+}
+
+void resetAlarmState() {
+  alarmMode = false;
+  settingMinutes = false;
+  settingHours = false;
+  showBootUp = false;
+  alarmReady = false;
+  mins = "";
+  hrs = "";
+}
+
+void displayClock() {
+  DateTime now = rtc.now();
   lcd.setCursor(0, 0);
   lcd.print("Time: ");
-  regular_clock_mode(currTime);
-
-
-  if (digitalRead(clk_reset_button) == 1) {
-    delay(1000);
-    reset_normal_clock();
-  }
-
-  else if (digitalRead(alarm_mode_button) == 1) {
-    delay(2000);
-    lcd.setCursor(0, 0);
-    lcd.print("Set alarm: ");
-    set_alarm();
-  }
+  lcd.setCursor(2, 1);
+  if (now.hour() < 10) lcd.print("0");
+  lcd.print(now.hour());
+  lcd.print(" : ");
+  if (now.minute() < 10) lcd.print("0");
+  lcd.print(now.minute());
+  lcd.print(" : ");
+  if (now.second() < 10) lcd.print("0");
+  lcd.print(now.second());
 }
 
-//if reset is high, set back the alarm to 00:00
-
-//if set_alarm is high, set clock to 00:00:00 and then make use of increment and decrement operations
-
-//if alarm_start is high, return to regular clock and countdown
-
-void reset_normal_clock() {
-  lcd.clear();
-  DateTime currTime = rtc.now();
-  rtc.adjust(DateTime(currTime.year(), currTime.month(), currTime.day(), currTime.hour(), currTime.minute(), currTime.second()));
+// ISR to mark alarm as triggered
+void alarmISR() {
+  alarmTriggered = true;
 }
 
-void set_alarm() {
-  DateTime currTime = rtc.now();
-  lcd.clear();
-  lcd.setCursor(1, 2);
-
-  bool flag = true;
-
-  int hrs = 0, mins = 0;
-  while (flag == true) {
-
-    // Update the display only when hours or minutes change
-    lcd.setCursor(1, 2);
-    lcd.print("Set Alarm: ");
-    if (hrs < 10) {
-      lcd.print("0");
-    }
-    lcd.print(hrs);
-    lcd.print(" : ");
-    if (mins < 10) {
-      lcd.print("0");
-    }
-    lcd.print(mins);
-
-    // Increment minutes
-    if (digitalRead(increment_button) == HIGH) {
-      mins++;
-      delay(200);  // debounce
-    }
-
-    // Decrement minutes
-    if (digitalRead(decrement_button) == HIGH) {
-      mins--;
-      delay(200);  // debounce
-    }
-
-    // Handle rollover for minutes and hours
-    if (mins == 60) {
-      mins = 0;
-      hrs++;
-    } else if (mins < 0) {
-      mins = 59;
-      hrs--;
-    }
-
-    // Handle rollover for hours
-    if (hrs == 24) {
-      hrs = 0;
-    } else if (hrs < 0) {
-      hrs = 23;
-    }
-
-    // Confirm the alarm setting
-    if (digitalRead(confirm_alarm_button) == HIGH) {
-      flag = false;
-    }
-  }
-
-  // Reset alarm time if the reset button is pressed
-  if (digitalRead(alarm_reset_button) == HIGH) {
-    hrs = 0;
-    mins = 0;
-  }
-
-  // Display the set alarm
+void soundAlarm() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Alarm set to ");
-  if (hrs < 10) {
-    lcd.print("0");
-  }
-  lcd.print(hrs);
-  lcd.print(" : ");
-  if (mins < 10) {
-    lcd.print("0");
-  }
-  lcd.print(mins);
+  lcd.print("WAKE UP!!!");
 
-  // Adjust the alarm time on the RTC module
-  rtc.setAlarm1(DateTime(currTime.year(), currTime.month(), currTime.day(), hrs, mins, 0), DS3231_A1_Hour);
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(buzzPin, HIGH);
+    delay(500);
+    digitalWrite(buzzPin, LOW);
+    delay(500);
+  }
+
+  // Clear alarm flag in RTC so it can be triggered again later
+  rtc.clearAlarm(1);
+
+  alarmTriggered = false;  // reset flag
+  lcd.clear();
 }
 
+void syncClock() {
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
 
-void regular_clock_mode(DateTime currTime) {
-  lcd.setCursor(2, 1);
-
-  currTime = rtc.now();
-  int hrs = currTime.hour(), mins = currTime.minute(), secs = currTime.second();
-
-  if (hrs < 10) {
-    lcd.print("0");
-  }
-  lcd.print(hrs);
-  lcd.print(" : ");
-
-  if (mins < 10) {
-    lcd.print("0");
-  }
-  lcd.print(mins);
-  lcd.print(" : ");
-
-  if (secs < 10) {
-    lcd.print("0");
-  }
-  lcd.print(secs);
-  delay(1000);
+void clearRow(int row) {
+  lcd.setCursor(0, row);
+  lcd.print("                    ");
 }
